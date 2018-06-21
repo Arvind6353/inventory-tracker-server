ad3d824340b6aa78e7265179bb0e8dc0772ff943
T A <arvta@paypal.com>
2018-06-21 17:17:03 +0530

    handle connection reset

--- db/config.js
+++ db/config.js
@@ -2,49 +2,35 @@ var mysql = require('mysql');
 var logger = require('../utils/logger')
 
 var connection = mysql.createConnection({
-  host: '10.176.16.106',
-   //host:'us-cdbr-iron-east-04.cleardb.net',
-  // host : 'localhost',
-  //port: '3301',
-  user: "admin",
- // user : "root",
-   //password : '',
-   //password: "Admin",
- password: "Admin1234!",
-  // database: 'maithree-db',
-  // password: "Admin",
-  // password: "0bbde678",
-   //user : "b8595257fb7838",
-   //password: "Admin1234!",
-   database: 'maithree-db',
+   // host: '10.176.16.106',
+    host:'us-cdbr-iron-east-04.cleardb.net',
+   // host : 'localhost',
+   //port: '3301',
+   //user: "admin",
+  // user : "root",
+    //password : '',
+    //password: "Admin",
+   // password: "Admin1234!",
+   // database: 'maithree-db',
+   // password: "Admin",
+    password: "0bbde678",
+    user : "b8595257fb7838",
+    //password: "Admin1234!",
+    //database: 'maithree-db',
 
-   //database: 'heroku_ae2a23a8d252231',
-   timezone: 'utc'
+    database: 'heroku_ae2a23a8d252231',
+    timezone: 'utc'
 });
 
 var createConnection = function() {
     
-   connection = mysql.createConnection({
-    host: '10.176.16.106',
-     //host:'us-cdbr-iron-east-04.cleardb.net',
-    // host : 'localhost',
-    //port: '3301',
-    user: "admin",
-   // user : "root",
-     //password : '',
-     //password: "Admin",
-   password: "Admin1234!",
-    // database: 'maithree-db',
-    // password: "Admin",
-    // password: "0bbde678",
-     //user : "b8595257fb7838",
-     //password: "Admin1234!",
-     database: 'maithree-db',
- 
-     //database: 'heroku_ae2a23a8d252231',
-     timezone: 'utc'
- });
-
+    
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("done");
+        logger.info('db connection successful')
+    });
+    
     connection.on('error', function(err) {
         console.log('db error', err);
         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
