c288b7fa518ca0c4fb3d8a770cdb22759517efa5
Nelson <sannelson@paypal.com>
2018-06-21 15:33:09 +0530

    config chg

--- db/config.js
+++ db/config.js
@@ -2,18 +2,23 @@ var mysql = require('mysql');
 var logger = require('../utils/logger')
 
 var connection = mysql.createConnection({
-    host: '10.176.16.106',
+   // host: '10.176.16.106',
+    host:'us-cdbr-iron-east-04.cleardb.net',
    // host : 'localhost',
-    //port: '3301',
-      user: "admin",
-    // user : "root",
+   //port: '3301',
+   //user: "admin",
+  // user : "root",
     //password : '',
     //password: "Admin",
    // password: "Admin1234!",
    // database: 'maithree-db',
    // password: "Admin",
-    password: "Admin1234!",
-    database: 'maithree-db',
+    password: "0bbde678",
+    user : "b8595257fb7838"
+    //password: "Admin1234!",
+    //database: 'maithree-db',
+
+    database: 'heroku_ae2a23a8d252231',
     timezone: 'utc'
 });
 
@@ -24,3 +29,4 @@ connection.connect(function (err) {
 });
 
 module.exports = connection;
+
