c31280ff9be05c5502228bc56bcc4346bc54c249
T A <arvta@paypal.com>
2018-06-21 17:06:31 +0530

    handle connection reset

--- db/config.js
+++ db/config.js
@@ -22,11 +22,27 @@ var connection = mysql.createConnection({
     timezone: 'utc'
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("done");
-    logger.info('db connection successful')
-});
+var createConnection = function() {
+    
+    
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("done");
+        logger.info('db connection successful')
+    });
+    
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          createConnection();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+          throw err;                                  // server variable configures this)
+        }
+      });
+    
+    }
 
+    createConnection();
+    
 module.exports = connection;
 
